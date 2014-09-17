30eb79f0a0c05bb25fe3fb18c856924659876494
Alec Geatches <throwpi@me.com>
2014-09-16 23:24:00 -0700

    Added combinable items and example to story

--- adventureFS/adventureFS.py
+++ adventureFS/adventureFS.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig()
 
 from collections import defaultdict
-from errno import ENOENT, EACCES, EFBIG
+from errno import ENOENT, EACCES, EFBIG, EPERM, EEXIST
 from stat import S_IFDIR, S_IFLNK, S_IFREG
 from sys import argv, exit
 from time import time
@@ -12,10 +12,38 @@ import os
 
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
+def setupStory():
+	west = Location('west', 'mountains and wind')
+	east = Location('east', 'shoreline and sand')
+	key = Item('key', 'skeleton key')
+	east.hold(key)
+
+	house = LockedLocation('house', 'old abandoned house')
+	house.lockWith(key)
+
+	battery = Item('battery', 'generic brand battery')
+	remote = Item('remote-control', 'lifeless plastic parasite')
+	poweredRemote = ActivatableItem('powered-remote-control', 'entertainment stick')
+	getCombiner().addFormula([battery, remote], poweredRemote)
+
+	def openAttic():
+		attic = Location('attic', 'dusty and dark')
+		note = Item('note', 'you win')
+		attic.hold(note)
+		house.hold(attic)
+	poweredRemote.action = openAttic
+
+	house.hold(battery)
+	house.hold(remote)
+	
+	root = Location('root')
+	root.hold(west, east, house)
+	return root
+
 class Item:
-	def __init__(self, name, description='An item\n'):
+	def __init__(self, name, description='plain item'):
 		self.name = name
-		self.description = description
+		self.description = description + '\n'
 		self.movable = True
 
 		now = time()
@@ -28,7 +56,7 @@ class Item:
 		return self._attributes
 
 class ActivatableItem(Item):
-	def __init__(self, name, description='An activatable item\n'):
+	def __init__(self, name, description='magic item'):
 		super(ActivatableItem, self).__init__(name, description)
 		self.action = None
 
@@ -99,30 +127,30 @@ def getInventory():
 	return getInventory.inventory
 getInventory.inventory = Inventory()
 
+class ItemCombiner:
+	def __init__(self):
+		self.formulae = defaultdict(None)
+
+	def addFormula(self, items, result):
+		self.formulae[self.setForItems(items)] = result
+
+	def canCombine(self, *items):
+		return self.setForItems(items) in self.formulae
+
+	def combine(self, *items):
+		return self.formulae.get(self.setForItems(items), None)
+
+	def setForItems(self, items):
+		return frozenset([i.description for i in items])
+
+def getCombiner():
+	return getCombiner.combiner
+getCombiner.combiner = ItemCombiner()
+
 class AdventureFS(LoggingMixIn, Operations):
 	def __init__(self):
-		west = Location('west', 'mountains and wind\n')
-		east = Location('east', 'shoreline and sand\n')
-		key = Item('key', 'A simple skeleton key\n')
-		east.hold(key)
-
-		house = LockedLocation('house', 'old abandoned house\n')
-		house.lockWith(key)
-
-		remoteControl = ActivatableItem('remote-control', 'a comically large remote control with one big button\n')
-		def openAttic():
-			attic = Location('attic', 'dusty and dark\n')
-			note = Item('note', 'You win!\n')
-			attic.hold(note)
-			house.hold(attic)
-		remoteControl.action = openAttic
-
-		house.hold(remoteControl)
-		
-		self.root = Location('root')
-		self.root.hold(west, east, house)
-
 		self.fd = 0
+		self.root = setupStory()
 
 	def access(self, path, mode):
 		location = self.getLocation(path)
@@ -161,11 +189,20 @@ class AdventureFS(LoggingMixIn, Operations):
 		if isinstance(item, Item):
 			if not item.movable:
 				raise FuseOSError(EACCES)
-			item.name = os.path.basename(newPath) 
 			oldParent = self.getParentLocation(oldPath)
 			newParent = self.getParentLocation(newPath)
-			oldParent.unhold(item)
-			newParent.hold(item)
+			combiningItem = self.getLocation(newPath)
+			if combiningItem is None:
+				item.name = os.path.basename(newPath) 
+				oldParent.unhold(item)
+				newParent.hold(item)
+			elif getCombiner().canCombine(item, combiningItem):
+				combined = getCombiner().combine(item, combiningItem)
+				oldParent.unhold(item)
+				newParent.unhold(combiningItem)
+				newParent.hold(combined)
+			else:
+				raise FuseOSError(EEXIST)
 		else:
 			raise FuseOSError(EFBIG)
 

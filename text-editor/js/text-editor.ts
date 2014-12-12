ae5b08fddc5e24c4b2992c60db4264eac74bb30e
Alec Geatches <throwpi@me.com>
2014-12-12 13:24:00 -0800

    Added Formatters and began buffer/display separation

--- text-editor/js/text-editor.ts
+++ text-editor/js/text-editor.ts
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
 	var textEditor = new TextEditor('#text-editor');
+	textEditor.setContent("int main() {\n\tprintf(\"Hello, world!\\n\");\n}");
 });
 
 class TextEditor {
@@ -11,7 +12,7 @@ class TextEditor {
 	constructor(selector: string) {
 		this.element = $(selector);
 
-		this.loadSavedContent();
+		// this.loadSavedContent();
 		this.setUpAutoSave();
 	}
 
@@ -28,5 +29,52 @@ class TextEditor {
 		    localStorage.setItem(TextEditor.localStorageKey, that.element.html());
 		    setTimeout(arguments.callee, 1000);
 		})();
+	}
+
+	setContent(content: string) {
+		var formatter = new MultiFormatter(new LineFormatter, new TabFormatter);
+		this.element.html(formatter.format(content));
+	}
+}
+
+interface Formatter {
+	format(content: string): string;
+}
+
+class MultiFormatter {
+	formatters: Formatter[]
+	constructor(...formatters: Formatter[]) {
+	    this.formatters = formatters;
+	}
+
+	format(content: string) {
+		for (var i in this.formatters) {
+			content = this.formatters[i].format(content);
+		}
+		return content;
+	}
+}
+
+class LineFormatter {
+	format(content: string) {
+		var lines = content.split(/\r?\n/);
+
+		var formatted = "";
+		for (var i in lines) {
+			formatted += this.formatLine(lines[i]);
+		}
+
+		return formatted;
+	}
+
+	formatLine(content: string) {
+		return "<div class=\"line\">" + content + "</div>";
+	}
+}
+
+class TabFormatter {
+	format(content: string) {
+		content = content.replace(/\t/g, '&nbsp;&nbsp;')
+		return content;
 	}
 }
